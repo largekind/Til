92cffca2c9dba529427264d4f692d99439d568a8
largekind <joboga1020@gmail.com>
2019-01-19 20:21:04 +0900

    Delete Downpng.py

--- downpng.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-
-#design url and save pass
-url = "http://uta.pw/shodou/img/28/214.png"
-savename = "tes.png"
-
-#download
-urllib.request.urlretrive(url,savename)
-print("Saved!\n")
