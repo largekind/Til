abe56da8e341c6bb70deb12fc4eea519f53068b1
largekind <joboga1020@gmail.com>
2019-02-01 16:26:55 +0900

    Add haykunin.py

---
+++ ch1/hyakunin.py
@@ -0,0 +1,29 @@
+#!/user/bin/env pythons3
+
+#read library
+import sys
+import urllib.request as req #urlib.requestライブラリをreq名で利用
+import urllib.parse as parse
+
+# コマンドライン引数取得 要sysモジュール
+if len(sys.argv) <= 1:
+   print("USAGE: hyakunin.py (keyword)") 
+   sys.exit()
+keyword = sys.argv[1]
+
+#パラメータにURIエンコード
+API = "http://api.aoikujira.com/hyakunin/get.pho"
+query = {
+    "fmt": "ini",
+    "key": keyword
+}
+params = parse.urlencode(query)
+url = API + "?" + params
+print("url=",url)
+
+# download
+# with文で初期処理~終了処理を自動化してもらう
+with req.urlopen(url) as r:
+   b = r.read()
+   data = b.encode('utf-8')
+   print(data)
