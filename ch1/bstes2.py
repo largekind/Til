2ac08dea9d62426f749e14337eff1e5bb21ce957
largekind <joboga1020@gmail.com>
2019-02-08 14:49:13 +0900

    Add betes2.py

---
+++ ch1/bstes2.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+
+html = """
+<html><body>
+    <h1>スクレイピングとは？</h1>
+    <p>Web ページを解析すること</p>
+    <p>任意の個所を抽出</p>
+</body></html>
+"""
+
+#HTML解析
+soup = BeautifulSoup(html,'html.parser')
+
+# findで取り出す
+title = soup.find(id = "title")
+body = soup.find(id = "body")
+
+#text 表示
+print("#titile ="+ title.string)
+print("#body" + body.string)
+
